5029e2c6f1ce342642c115e46add0541ddde8c46
Nicola Riker <39339887+nicolascribbles@users.noreply.github.com>
2024-09-27 01:39:17 -0600

    created sketchbook ui layout

--- packages/library/src/App.vue
+++ packages/library/src/App.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import SketchbookUILayout from './layouts/SketchbookUILayout.vue'
+
+const addDarkModeClassToBody = () => {
+  document.body.classList.toggle('dark')
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <SketchbookUILayout>
+    <div class="container">
+      Hello!<br />
+      Gonna make this longer <br /><br />
+    </div>
+    <template #sidebar>
+      <div class="container">
+        Testing the sidebar!
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+        <button @click="addDarkModeClassToBody">
+          Toggle Dark Mode
+        </button>
+      </div>
+    </template>
+  </SketchbookUILayout>
+</template>
